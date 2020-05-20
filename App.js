a2b7dd4d6de4e0195f5f62ad10cc263f2aee7115
c1297980 Joao Victor Holanda Roriz <c1297980@interno.bb.com.br>
2020-05-20 19:56:34 -0300

    alteracoes push

--- App.js
+++ App.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import {Alert, Platform} from 'react-native';
+import Home from './screen/Home';
 
-import Home from './screen/Home'
+import {Notifications} from 'react-native-notifications';
+import messaging from '@react-native-firebase/messaging';
+import {
+  requestUserPermission,
+  registerNotificationHandler,
+} from './services/notification';
+
+requestUserPermission();
+registerNotificationHandler();
+
+let localNotification = Notifications.postLocalNotification({
+  body: 'Local notification!',
+  title: 'Local Notification Title',
+  silent: false,
+  category: 'SOME_CATEGORY',
+  userInfo: {},
+});
 
 const App = () => {
-	return <Home />
-}
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
 
-export default App
+    messaging().onNotificationOpenedApp((remoteMessage) => {
+      'Notification caused app to open from background state:',
+        console.log(remoteMessage.notification);
+    });
+
+    // Check whether an initial notification is available
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+        }
+      });
+
+    return unsubscribe;
+  }, []);
+
+  return <Home />;
+};
+
+export default App;
